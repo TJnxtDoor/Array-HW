3c3f9b208f42503628d7e4c2334024419185ee42
TJ <afzaltj81@gmail.com>
2026-03-18 09:48:06 -0400

    created car array
    the build is running

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Carsr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = {"car1", "car2", "car3", "car4"};
+            for(int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+        }
+    }
+}
